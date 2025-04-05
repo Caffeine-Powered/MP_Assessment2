d18ab6c156009a8156875cde1e343769283bce3b
Caffeine-Powered <wmedwards96@gmail.com>
2025-04-05 19:06:09 +0100

    asdsdasdas

---
+++ Assets/Scripts/ChangeSceneBack.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeSceneBack : MonoBehaviour
+{
+private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Token")
+        {
+            //gameObject.GetComponent<AudioSource>().PlayOneShot(zombiesAudio);
+            SceneManager.LoadSceneAsync(0);
+        }
+    }
+}
